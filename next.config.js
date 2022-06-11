ef277716c48641e469d0cb93a2bd6e389655a8e4
0xsol1id <93030247+0xsol1id@users.noreply.github.com>
2022-06-11 11:53:19 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -77,10 +77,10 @@ module.exports =
     return [
       {
         // matching all API routes
-        source: "/.*",
+        source: "/v2/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://api-mainnet.magiceden.dev" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
